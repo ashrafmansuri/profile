f42454c97f7825c9bc550e3d9198a04e9c979178
ashrafmansuri <ashrafmansuri@outlook.com>
2023-08-14 14:36:20 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Ashraf Mansuri - Senior Software Developer</title>
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+        <link href="css/fontawesome.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                <span class="d-block d-lg-none">Ashraf Mansuri</span>
+                <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="assets/img/profile.jpg" alt="..." /></span>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Education</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">Interests</a></li>
+                    
+                </ul>
+            </div>
+        </nav>
+        <!-- Page Content-->
+        <div class="container-fluid p-0">
+            <!-- About-->
+            <section class="resume-section" id="about">
+                <div class="resume-section-content">
+                    <h1 class="mb-0">
+                        Ashraf
+                        <span class="text-primary">Mansuri</span>
+                    </h1>
+                    <div class="subheading mb-5">
+                        Senior Software Developer · Mumbai ·
+                        <a href="mailto:name@email.com">mail@ashrafmansuri.in</a>
+                    </div>
+                    <p class="lead mb-5">Highly skilled and dedicated .NET Developer with 4+ years of experience, seeking a challenging position where I can
+utilize my expertise in designing and developing robust applications, implementing new features, and resolving bugs.
+Committed to delivering high-quality software solutions that meet business needs and enhance user experiences.</p>
+                    <div class="social-icons">
+                        <a class="social-icon" href="https://www.linkedin.com/in/mansuriashraf/"><i class="fab fa-linkedin-in"></i></a>
+                        <a class="social-icon" href="https://github.com/ashrafmansuri"><i class="fab fa-github"></i></a>
+                        <a class="social-icon" href="https://www.facebook.com/GetToKnowAshraf/"><i class="fab fa-facebook-f"></i></a>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Experience-->
+            <section class="resume-section" id="experience">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Experience</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Senior Software Developer</h3>
+                            <div class="subheading mb-3">We3 Tech Works - (Client : Motilal Oswal Financial Services Limited)</div>
+                            <p>Played a crucial role in the core team of CBOS (Core Back Office System), a comprehensive software solution for managing back-office operations at a leading stockbroking firm. Developed expertise in .NET Core, SQL, SQL CLR, and frameworks like JQuery, Dapper and iTextSharp. Collaborated with clients, users, and team members to gather requirements, prioritize features, and design solutions that met business needs and compliance standards.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">August 2022 – Present</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Software Programming Associate</h3>
+                            <div class="subheading mb-3">HSNCB</div>
+                            <p>An educational trust currently administrating 25+ prominent institutes which includes colleges like KC College, HR College and more. Development and Testing. Developed, Tested and Deployed web applications that are now used by the vast web of institutions. Configured and managed custom server infrastructure. Suggested and implemented a variety of ideas which helped in enhancing the application as well as user experience</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">June 2021 – July 2022</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Software Developer</h3>
+                            <div class="subheading mb-3">Scan Tools Solutions</div>
+                            <p>Digitized the working system by developing applications from scratch making it a more streamlined process and time and revenue saving. Designed, programmed and manufactured tools for the Indian Automotive Electronic Market. Successfully managed to expand the product range by negotiating deals with foreign manufactures increasing sales up to 3x.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">August 2019 - May 2021</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Trainee Intern</h3>
+                            <div class="subheading mb-3">R. D. National & W. A. Science College</div>
+                            <p>Selected on basis of the final year project which got adopted by the college. Developed, Tested and Troubleshot applications on .NET Framework and MS SQL used by the college in every aspect of operation. Frequently coordinated with the staff, attending their queries, issues and experience and integrated the necessary changes to the ERP. Developed and Implemented programs which analyzed and generated reports on data.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">July 2017 – July 2019</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Education-->
+            <section class="resume-section" id="education">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Education</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">R. D. National & W. A. Science College, Affiliated to the University of Mumbai</h3>
+                            <div class="subheading mb-3">Bachelor of Science</div>
+                            <div>Computer Science</div>
+                            <p>GPA: 5.53</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">2014 – 2017</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Rizvi College of Arts, Science and Commerce</h3>
+                            <div class="subheading mb-3">H.S.C.</div>
+                            <p>54.00%</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">2012 - 2014</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Little Angels English High School</h3>
+                            <div class="subheading mb-3">S.S.C.</div>
+                            <p>69.64%</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">2001 – 2012</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Skills-->
+            <section class="resume-section" id="skills">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Skills</h2>
+                    <div class="subheading mb-3">Programming Languages & Tools</div>
+                    <ul class="list-inline dev-icons">
+                        <li class="list-inline-item"><i class="fa fa-code"></i></li>
+                        <li class="list-inline-item"><i class="fa fa-solid fa-server"></i></li>
+                        <li class="list-inline-item"><i class="fa-brands fa-html5"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
+                        <li class="list-inline-item"><i class="fa-solid fa-terminal"></i></li>
+                        <li class="list-inline-item"><i class="fa-solid fa-c"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-linux"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-java"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-php"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-google"></i></li>
+                        <li class="list-inline-item"><i class="fa-brands fa-git"></i></li>
+                        <li class="list-inline-item"><i class="fa-solid fa-cloud"></i></li>
+                      
+                    </ul>
+                    <div class="subheading mb-3">Workflow</div>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Planning and Design
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Development
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Testing & Debugging
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Optimization for performance
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Agile Development & Scrum
+                        </li>
+                    </ul>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Interests-->
+            <section class="resume-section" id="interests">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Interests</h2>
+                    <p>During my free time, I enjoy a variety of hobbies that help me see the world in different ways. I watch sci-fi and fantasy genre movies and television shows, read different kinds of books, play games, and find happiness in funny things. These hobbies show that I'm always eager to learn new stuff. </p>
+                    <p class="mb-0">Plus, I like exploring cafes and trying out tasty food. I spend a large amount of my free time exploring the latest technology advancements in the development world. These things show that I'm a curious person who keeps growing and can connect with others who like different things.</p>
+                </div>
+            </section>
+            
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
